8a1ab52d034f9a77184845f503d5d72cce3eecd6
arielGs_Dev <arielogavilanes1534@gmail.com>
2024-05-06 21:40:27 -0500

    modulos de auth

--- src/app/auth/register/register.module.ts
+++ src/app/auth/register/register.module.ts
@@ -1,15 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RegisterComponent } from './register.component';
 
-import { AuthRoutingModule } from './auth-routing.module';
 
 
 @NgModule({
   declarations: [
+    RegisterComponent
   ],
   imports: [
-    CommonModule,
-    AuthRoutingModule
+    CommonModule
   ]
 })
-export class AuthModule { }
+export class RegisterModule { }
