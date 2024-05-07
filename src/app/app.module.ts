2ca14110cb49ad2f02a29eb60d1a6ccbdf3c0b97
Danitarapues2 <dpr.tarapues@yavirac.edu.ec>
2024-05-06 23:16:01 -0500

    Diseño en shared Funcando

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { SharedComponent } from './shared/shared.component';
+import { ButtonModule } from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SharedComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
+import { SharedComponent } from './shared.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonModule } from 'primeng/button';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    SharedComponent,
+    BrowserModule,
+    BrowserAnimationsModule
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    ButtonModule
   ]
 })
 export class SharedModule { }
